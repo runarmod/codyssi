b1cb25ad2cd76d7237f938df10a1ad778652fd85
Runar Saur Modahl <runarmod@gmail.com>
2025-04-27 13:42:29 +0200

    2025 18 optimized

--- 2025/18/main.py
+++ 2025/18/main.py
@@ -1,11 +1,9 @@
-import heapq
 import itertools
+import math
 import os
 import re
 import time
 from dataclasses import dataclass
-
-from more_itertools import zip_equal
 
 
 @dataclass
@@ -41,86 +39,85 @@ class Solution:
                 Rule, map(nums, get_data(test=test).strip("\n").split("\n"))
             )
         )
-        self.dimensions = (10, 15, 60, 3)
-        self.ranges = list(map(range, self.dimensions[:-1])) + [range(-1, 2)]
+        dimensions = (10, 15, 60, 3)
+        self.X, self.Y, self.Z, self.A = dimensions
+        self.ZA = self.A * self.Z
+        self.YZA = self.ZA * self.Y
+        self.XYZA = self.YZA * self.X
 
-    def count_debris(
-        self,
-        coord: tuple[int, int, int, int],
-        time: int,
-        allowed_hits: int = 9999999,
-    ) -> bool:
-        count = 0
-        if coord == (0, 0, 0, 0):
-            return count
+        self.time_steps = math.lcm(*dimensions)
+        self.debris = self.precompute_debris()
+
+    def idx(self, x: int, y: int, z: int, a: int):
+        return x * self.YZA + y * self.ZA + z * self.A + (a + 1)
+
+    def r_idx(self, xyza: int):
+        x, yza = divmod(xyza, self.YZA)
+        y, za = divmod(yza, self.ZA)
+        z, a = divmod(za, self.A)
+        return x, y, z, a - 1
+
+    def precompute_debris(self):
+        debris: list[int] = [[0] * self.XYZA for _ in range(self.time_steps)]
+
         for rule in self.rules:
-            time_aware_coord = [
-                coord[0] - rule.vx * time,
-                coord[1] - rule.vy * time,
-                coord[2] - rule.vz * time,
-                coord[3] - rule.va * time,
-            ]
-            time_aware_coord = [
-                *(
-                    custom_mod(c, 0, self.dimensions[i] - 1)
-                    for i, c in enumerate(time_aware_coord[:-1])
-                ),
-                custom_mod(time_aware_coord[-1], -1, 1),
-            ]
-            if (
-                rule.x * time_aware_coord[0]
-                + rule.y * time_aware_coord[1]
-                + rule.z * time_aware_coord[2]
-                + rule.a * time_aware_coord[3]
-            ) % rule.divide == rule.remainder:
-                count += 1
-                if count > allowed_hits:
-                    return count
-        return count
+            for xyza in range(self.XYZA):
+                x, y, z, a = self.r_idx(xyza)
+                if (
+                    rule.x * x + rule.y * y + rule.z * z + rule.a * a
+                ) % rule.divide == rule.remainder:
+                    debris[0][self.idx(x, y, z, a)] += 1
+                    for t in range(1, self.time_steps):
+                        x = (x + rule.vx) % self.X
+                        y = (y + rule.vy) % self.Y
+                        z = (z + rule.vz) % self.Z
+                        a = (a + rule.va + 1) % self.A - 1
+                        debris[t][self.idx(x, y, z, a)] += 1
+
+        return debris
+
+    def count_debris(self, xyza: int, time: int) -> bool:
+        if xyza == self.idx(0, 0, 0, 0):
+            return 0
+        return self.debris[time % self.time_steps][xyza]
 
     def part1(self):
-        return sum(
-            self.count_debris(coord, 0) for coord in itertools.product(*self.ranges)
-        )
+        return sum(self.count_debris(xyza, 0) for xyza in range(self.XYZA))
 
-    def A_star(self, start_lives: int):
-        start = (0, 0, 0, 0)
-        goal = tuple([*(x - 1 for x in self.dimensions[:-1]), 0])
-        q = [(0, 0, start_lives, start)]  # (estimate, time_used, lives, coord)
-        visited = {}  # (time, coord): lives
-        while q:
-            _, time, lives, coord = heapq.heappop(q)
-            if coord == goal:
-                return time
+    def calculate(self, allowed_hits: int):
+        start = self.idx(0, 0, 0, 0)
+        goal = self.idx(self.X - 1, self.Y - 1, self.Z - 1, 0)
 
-            if visited.get((time, coord), 0) >= lives:
-                continue  # We have been in this state before, or better (in regards to lives)
-            visited[time, coord] = lives
-
-            new_time = time + 1
-            # Do not include the last dimension (only debris can use it)
-            for neighbor in neighbors4(coord[:-1]):
-                neighbor = tuple((*neighbor, 0))
-                if not all(0 <= n < d for n, d in zip_equal(neighbor, self.dimensions)):
+        hits = [allowed_hits + 1] * self.XYZA
+        hits[start] = 0
+        t = 0
+        while hits[goal] > allowed_hits:
+            t += 1
+            new_hits = hits[:]
+            for xyza, h in enumerate(hits):
+                if h > allowed_hits:
                     continue
-                new_lives = lives - self.count_debris(neighbor, new_time, lives)
-                if new_lives <= 0:  # Dead
-                    continue
-                estimate = manhattan(neighbor, goal) + new_time
-                heapq.heappush(q, (estimate, new_time, new_lives, neighbor))
+                x, y, z, _ = self.r_idx(xyza)  # a is guaranteed 0
+                for i, neighbor in neighbors4((x, y, z)):
+                    if not 0 <= neighbor[i] < (self.X, self.Y, self.Z)[i]:
+                        continue
 
-            # Maybe do not move
-            new_lives = lives - self.count_debris(coord, new_time, lives)
-            if new_lives > 0:  # Still alive
-                estimate = manhattan(neighbor, goal) + new_time
-                heapq.heappush(q, (estimate, new_time, new_lives, coord))
-        return None
+                    neighbor = self.idx(*neighbor, 0)
+                    new_hits[neighbor] = min(new_hits[neighbor], h)
+
+            for xyza, h in enumerate(self.debris[t % self.time_steps]):
+                new_hits[xyza] += h
+
+            new_hits[start] = 0
+            hits = new_hits
+
+        return t
 
     def part2(self):
-        return self.A_star(1)
+        return self.calculate(0)
 
     def part3(self):
-        return self.A_star(4)
+        return self.calculate(3)
 
 
 def main():
@@ -142,23 +139,10 @@ def main():
     print(f"\nTotal time: {time.perf_counter() - start: .4f} sec")
 
 
-def custom_mod(num: int, low: int, high: int) -> int:
-    range_size = high - low + 1
-    while num < low:
-        num += range_size
-    while num > high:
-        num -= range_size
-    return num
-
-
 def neighbors4(point: tuple[int, ...], jump=1):
     for i in range(len(point)):
         for diff in (-jump, jump):
-            yield point[:i] + (point[i] + diff,) + point[i + 1 :]
-
-
-def manhattan(p1: tuple[int, ...], p2: tuple[int, ...]):
-    return sum(abs(a - b) for a, b in zip_equal(p1, p2))
+            yield i, point[:i] + (point[i] + diff,) + point[i + 1 :]
 
 
 if __name__ == "__main__":
